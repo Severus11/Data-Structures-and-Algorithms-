a8f654b856d6d1b0ffe4fe9c08d26b1516eb412a
Severus11 <36445722+Severus11@users.noreply.github.com>
2020-07-08 02:20:56 +0530

    Add files via upload

--- Trees/binaryT.cpp
+++ Trees/binaryT.cpp
@@ -62,19 +62,33 @@ void post_order(Node *root)
         cout<<root->data<<" ";
     }
 }
-
+void level_order(Node* root, int i)
+{
+	if (root !=0)
+	{
+		cout<<root->data<<" ";
+		level_order(root->left , 2*i+1);
+		level_order(root->right , 2*i+2);
+		i++;
+	}
+}
 int main()
 {
     int arr[] = {1,2,3,4,5,6,7,8,9};
     int n = sizeof(arr)/sizeof(arr[0]);
     Node * root= ins(arr, root, 0, n);
-    pre_order(root);
+    cout<<"Pre-Order Traversal of the tree is:"<<endl;
+	pre_order(root);
     cout<<"\n";
+	cout<<"\n"<<"post-Order Traversal of the tree is:"<<endl;
     post_order(root);
     cout<<"\n";
+	cout<<"\n"<<"in-Order Traversal of the tree is:"<<endl;
     in_order(root);
     cout<<"\n";
-
+	cout<<"\n"<<"level-Order Traversal of the tree is:"<<endl;
+	level_order(root, 0);
+	cout<<"\n";
     return 0;
 }
 

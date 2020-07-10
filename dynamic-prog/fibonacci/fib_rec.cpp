ebddab6aacb6a7bd5243b9bffc479f5425c1167e
Severus11 <36445722+Severus11@users.noreply.github.com>
2020-07-10 14:55:18 +0530

    Add files via upload

---
+++ dynamic-prog/fibonacci/fib_rec.cpp
@@ -0,0 +1,21 @@
+//this is the general recursive approach. 
+//Time complexity is o(n2) and space complexity is also o(n2)
+
+#include<iostream>
+
+using namespace std;
+
+int fib(int n)
+{
+    if (n<=1) return n;
+
+    return fib(n-1) + fib(n-2);
+}
+
+int main()
+{
+    int n;
+    cout<<"enter the number: "; 
+    cin>>n;
+    cout<<fib(n);
+}

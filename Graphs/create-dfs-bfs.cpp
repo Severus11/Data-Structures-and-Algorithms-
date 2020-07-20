20b66c279c9700339b50652f2f1d682700deff68
Severus11 <parthsgupta@gmail.com>
2020-07-20 22:50:25 +0530

    init

---
+++ Graphs/create-dfs-bfs.cpp
@@ -0,0 +1,7 @@
+//graphs can be visualised as trees with interconnects
+// graphs can be modelled as adjacency matrix or adjacency lists.
+// BFS is like level order traversal and DFS is like preorder traverssal
+#include<iostream>
+
+using namespace std;
+

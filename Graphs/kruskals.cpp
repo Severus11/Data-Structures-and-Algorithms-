a3acfc417e17575e1f7c8cdd23f0c05cddb1e58c
Severus11 <parthsgupta@gmail.com>
2020-07-30 23:57:05 +0530

    added kruskal

---
+++ Graphs/kruskals.cpp
@@ -0,0 +1,7 @@
+//kruskal says, take a minimum cost edge irresptive of being connected or not.
+// if a cycle is forming, skip it 
+//prim focussed on finding a tree. kruskal focusses on finding miniumum tree
+#include<bits/stdc++.h>
+#define I INT_MAX
+
+int set

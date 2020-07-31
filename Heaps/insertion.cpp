5b0ff6348dedccef0907c75e7225d67640ad1031
Severus11 <parthsgupta@gmail.com>
2020-07-31 17:23:57 +0530

    added heap

---
+++ Heaps/insertion.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insert(int a[], int n)
+{
+    int temp, i=n;
+    temp = a[n];
+    while(i>1 && temp> a[i/2])
+    {
+        a[i] = a[i/2];
+        i = i/2;
+    }
+    a[i] = temp;
+}
+void createheap(int a[], int n)
+{
+    for (int i=2; i<n ;i++)
+    {
+        insert(a, i);
+    }
+}
+int main()
+{
+    int arr[]= {0,10,20,30,25,5,40,35};
+    createheap(arr,8);
+    for(int i=0; i<8; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
---
+++ Heaps/readme.md
@@ -0,0 +1,9 @@
+## Biinary Heaps
+### Heaps are complete binary trees, satisfying one of these conditions:
+- 1. Max Heap-  where the descendents of the parent are lesser than or equal to it.
+- 2. Min Heap- The descendents are greater than or equal to the parent.
+
+- Heaps are generally represented using arrays
+- Height is always logn
+- not used for searching purpose
+
